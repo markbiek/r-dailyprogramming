404cba7efb63f99f377d1f43d30c96b74e3e04dd
Mark Biek <support@janustech.net>
2014-04-10 11:56:28 -0400

    Solved an easy one

---
+++ 156-decoder.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+"""
+http://www.reddit.com/r/dailyprogrammer/comments/21yuep/422014_challenge_156_intermediate_managing_workers/
+"""
+
+def decodeC(c):
+    if c == '\n' or c == '\r':
+        return c
+    else:
+        return chr(ord(c)-4)
+
+def decode(s):
+    return "".join( [decodeC(c) for c in msg] )
+
+msg = """Etvmp$Jsspw%%%%
+[e}$xs$ks%$]sy$lezi$wspzih$xli$lmhhir$qiwweki2$Rs{$mx$mw$}syv$xyvr$xs$nsmr
+mr$sr$xlmw$tvero2$Hs$rsx$tswx$er}xlmrk$xlex${mpp$kmzi$e{e}$xlmw$qiwweki2$Pix
+tistpi$higshi$xli$qiwweki$sr$xlimv$s{r$erh$vieh$xlmw$qiwweki2$]sy$ger$tpe}$epsrk
+f}$RSX$tswxmrk$ls{$}sy$higshih$xlmw$qiwweki2$Mrwxieh$tswx$}syv$wspyxmsr$xs$fi$}syv
+jezsvmxi$Lipps${svph$tvskveq$mr$sri$perkyeki$sj$}syv$glsmgi2$
+Qeoi$wyvi$}syv$tvskveq$we}w$&Lipps$[svph%%%&${mxl$7$%$ex$xli$irh2$Xlmw${e}
+tistpi$fvs{wmrk$xli$gleppirki${mpp$xlmro${i$lezi$epp$pswx$syv$qmrhw2$Xlswi${ls$tswx$lipps
+{svph$wspyxmsrw${mxlsyx$xli$xlvii$%%%${mpp$lezi$rsx$higshih$xli$qiwweki$erh$ws$}sy$ger$
+tspmxip}$tsmrx$syx$xlimv$wspyxmsr$mw$mr$ivvsv$,xli}$evi$nywx$jspps{mrk$epsrk${mxlsyx$ors{mrk-
+Irns}$xlmw$jyr2$Xli$xvyxl${mpp$fi$liph$f}$xlswi${ls$ger$higshi$xli$qiwweki2$>-"""
+
+print decode(msg)
