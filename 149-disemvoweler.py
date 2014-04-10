4f67973bb0b42cdd6b436c7f8a1d96c9ca09ba7b
Mark Biek <support@janustech.net>
2014-04-10 15:13:20 -0400

    Modified version that doesn't use re

--- 149-disemvoweler.py
+++ 149-disemvoweler.py
@@ -3,12 +3,25 @@
 import re
 
 def disemvowel(s):
-    nonv = re.sub(r'[^aeiou]', '', s)
-    v = re.sub(r'[aeiou ]', '', s)
-    print v
+    v = re.sub(r'[^aeiou]', '', s)
+    nonv = re.sub(r'[aeiou ]', '', s)
     print nonv
+    print v
+    print ""
+
+def disemvowel2(s):
+    v = [ c for c in s if c in 'aeiou' ]
+    nonv = [ c for c in s if not c in 'aeiou ' ]
+    print "".join(nonv)
+    print "".join(v)
     print ""
 
 disemvowel("two drums and a cymbal fall off a cliff")
+disemvowel2("two drums and a cymbal fall off a cliff")
+
 disemvowel("all those who believe in psychokinesis raise my hand");
+disemvowel2("all those who believe in psychokinesis raise my hand");
+
 disemvowel("did you hear about the excellent farmer who was outstanding in his field")
+disemvowel2("did you hear about the excellent farmer who was outstanding in his field")
+
